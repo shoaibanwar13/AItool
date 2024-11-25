bcbbb0e66052b5c8e8871ddc80242f0f0a30e364
Shoaib Anwar <shoaib4311859@gmail.com>
2024-11-26 02:27:20 +0500

    googlelogin

--- Humanizer/urls.py
+++ Humanizer/urls.py
@@ -24,6 +24,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("humantext.urls")),
     path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('accounts/', include('allauth.urls')),  # Required for social account signup
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+from django.http import HttpResponse
+from django.urls import path
+def social_signup_placeholder(request):
+    return HttpResponse("Custom social signup not implemented yet.", status=501)
+
+urlpatterns += [
+    path('accounts/social/signup/', social_signup_placeholder, name='socialaccount_signup'),
+]
