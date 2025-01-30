ac2754eebded44b26cc5fcbe2a3b94018729d1ca
Shoaib Anwar <shoaib4311859@gmail.com>
2025-01-30 12:58:54 +0500

    finall

--- humantext/urls.py
+++ humantext/urls.py
@@ -12,35 +12,24 @@ from rest_framework_simplejwt.views import (
  
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/',  RegisterView.as_view(), name='register'),
-    path('verify/', RegisterVerifyOTPView.as_view(), name='verify-otp'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
-    path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
-    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('plans/', get_all_plans, name='get_all_plans'),
-    #path('plan/<int:plan_id>/',PlanDetail.as_view(),name="PlanDetail"),
-    path('plan-status/', check_plan_status, name='check_plan_status'),
-    path('create-payment-intent/', create_payment_intent, name='create-payment-intent'),
-    #path("paymentpage/",paymentpage,name="paymentpage"),
-    #path('paypal/create/', PaypalPaymentView.as_view(), name='ordercreate'),
-    #path('paypal/validate/', PaypalValidatePaymentView.as_view(), name='paypalvalidate'),
-    path('my-profile/', GetProfileView.as_view(), name='get-profile'),
-    path('edit-profile/', ProfileUpdateView.as_view(), name='profile-update'),
-    path('contact/',ContactUsView.as_view(),name='contact'),
-    path('verify_payment/',verify_payment,name="verify_payment"),
-    path('planexpired/',PlanExpiryHundler.as_view(),name="planexpired"),
-    path('generate/', HixAPIHandler.as_view(), name='hix-api'),
-    #path('generate/', GenerateTextView.as_view(), name='generate_text'),
-    path("ai-detector/",ai_detector,name="ai_detector")
-    
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register', RegisterView.as_view(), name='register'),
+    path('api/verify', RegisterVerifyOTPView.as_view(), name='verify-otp'),
+    path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/login', LoginView.as_view(), name='login'),
+    path('api/forgot-password', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('api/verify-otp', VerifyOTPView.as_view(), name='verify-otp'),
+    path('api/reset-password', PasswordResetView.as_view(), name='reset-password'),
+    path('api/change-password', ChangePasswordView.as_view(), name='change-password'),
+    path('api/plans', get_all_plans, name='get_all_plans'),
+    path('api/plan-status', check_plan_status, name='check_plan_status'),
+    path('api/create-payment-intent', create_payment_intent, name='create-payment-intent'),
+    path('api/my-profile', GetProfileView.as_view(), name='get-profile'),
+    path('api/edit-profile', ProfileUpdateView.as_view(), name='profile-update'),
+    path('api/contact', ContactUsView.as_view(), name='contact'),
+    path('api/verify_payment', verify_payment, name="verify_payment"),
+    path('api/planexpired', PlanExpiryHundler.as_view(), name="planexpired"),
+    path('api/generate', HixAPIHandler.as_view(), name='hix-api'),
+    path('api/ai-detector', ai_detector, name="ai_detector"),
 ]
-    
-
-
- 
