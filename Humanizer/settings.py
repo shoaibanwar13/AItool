57e4b9273401faed21007edd552942e3a59d1ccd
Shoaib Anwar <124421743+shoaibanwar13@users.noreply.github.com>
2025-01-31 11:17:49 +0500

    smt config

--- Humanizer/settings.py
+++ Humanizer/settings.py
@@ -205,8 +205,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD =os.getenv('EMAIL_HOST_PASSWORD') 
+EMAIL_HOST_USER = "shoaib0033237@gmail.com"
+EMAIL_HOST_PASSWORD = "tpij vqii jyyi yveo"
 
 LANGUAGE_CODE = 'en-us'
 
